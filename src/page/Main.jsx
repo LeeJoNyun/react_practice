7f1ddb0a9dea2d35c69f1c3e3a05c871c0df67b8
jlee <115512931+LeeJoNyun@users.noreply.github.com>
2025-07-09 14:45:48 +0900

    .2

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,7 +1,7 @@
 import Visual from "../components/main/visual/Visual";
-import Content1 from "../components/main/Content1/Content1";
+import Content1 from "../components/main/content1/Content1";
 import Content2 from "../components/main/content2/Content2";
-import Content3 from "../components/main/Content3/Content3";
+import Content3 from "../components/main/content3/Content3";
 
 const Main = () => {
   return (
